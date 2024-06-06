bb6608882788d9a388b2f5cd18a0f84eeafec3d7
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-06 08:49:15 -0500

    Solucionando errores de merge, git no puedo solucionar estos errores, consta en conflictos de versiones

--- listaContactos/personas/models.py
+++ listaContactos/personas/models.py
@@ -8,9 +8,5 @@ class Persona(models.Model):
     edad      = models.IntegerField()
     email     = models.CharField(max_length = 100)
     donador   = models.BooleanField(default=False)
-<<<<<<< HEAD
-    dni       = models.CharField(max_length = 8, default="--------")
-=======
     dni       = models.CharField(max_length = 8, default="--------")
     telefono  = models.CharField(max_length = 9, default="---------")
->>>>>>> pruebas
