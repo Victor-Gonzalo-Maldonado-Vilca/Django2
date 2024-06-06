aa276102849ee30365f2d5314115d2acf1ec3b36
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-06 08:39:18 -0500

    Añadiendo nuevo campo telefono, Prueba para confirmar que si se requiere un dato por defecto sino no se podra actualizar los cambios en la base de datos

--- listaContactos/personas/models.py
+++ listaContactos/personas/models.py
@@ -3,7 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Persona(models.Model):
-    nombre    = models.charField(max_length = 100)
-    apellidos = models.charField(max_length = 100)
-    edad      = models.IntegerField(max_digits = 3)
-    email     = models.charField(max_length = 100)
+    nombre    = models.CharField(max_length = 100)
+    apellidos = models.CharField(max_length = 100)
+    edad      = models.IntegerField()
+    email     = models.CharField(max_length = 100)
+    donador   = models.BooleanField(default=False)
+    dni       = models.CharField(max_length = 8, default="--------")
+    telefono  = models.CharField(max_length = 9, default="---------")
