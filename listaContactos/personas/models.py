cfbbf7228740f7c5bf5dc3834d48e56e17b8329d
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-06 06:53:28 -0500

    Añadiendo modelo Persona con los campos nombre, apellidos y edad

--- listaContactos/personas/models.py
+++ listaContactos/personas/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Persona(models.Model):
+    nombre    = models.charField(max_length = 100)
+    apellidos = models.charField(max_length = 100)
+    edad      = models.IntegerField(max_digits = 3)
