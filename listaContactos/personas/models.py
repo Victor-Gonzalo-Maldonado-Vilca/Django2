13c17bbf6834abab28d88b4214ac6a75efec9868
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-06 08:54:50 -0500

    ¿Cómo crearía un campo que sea obligatorio?, Se hizo una prueba donde los campos nombre y apellido son obligatorios, entonces la respuesta es: se crearía un campo obligatorio con blank=False

--- listaContactos/personas/models.py
+++ listaContactos/personas/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Persona(models.Model):
-    nombre    = models.CharField(max_length = 100)
-    apellidos = models.CharField(max_length = 100)
+    nombre    = models.CharField(max_length = 100, blank=False)
+    apellidos = models.CharField(max_length = 100, blank=False)
     edad      = models.IntegerField()
     email     = models.CharField(max_length = 100)
     donador   = models.BooleanField(default=False)
