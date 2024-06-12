b1998d19d5cd625f0d7300db156375c3e5b1bae0
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-12 11:47:47 -0500

    Definiendo vista personaView en views.py donde se obtendra informacion de la base de datos y se contendra en los elementos del diccionarios (nombre y edad), la información se enviara a la plantilla test/html

--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Persona
 
 # Create your views here.
+
+def personaView(request):
+    obj = Persona.objects.get(id = 1)
+    context = {
+        'nombre': obj.nombres,
+        'edad': obj.edad,
+    }
+    return render(request, 'personas/test.html', context)
