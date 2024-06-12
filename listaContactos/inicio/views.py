ba5a8dc39141f1c275dfec37aad9596c3caad4f3
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-12 09:45:53 -0500

    Haciendo prueba con la variable predefinida forloop.counter0 se puede observar que todos el coteo de elementos inicia desde 0

---
+++ listaContactos/inicio/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def myHomeView(request, *args, **kwargs):
+    myContext = {
+        'myText': 'Esto es sobre nosostros',
+        'myNumber': 123,
+        'myList': [33, 44, 55],
+    }
+    return render(request, "home.html", myContext)
+    
+def anotherView(request):
+    return HttpResponse('<h1>Solo otra página</h1>')
--- listaContactos/listaContactos/urls.py
+++ listaContactos/listaContactos/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from inicio.views import myHomeView, anotherView
 
 urlpatterns = [
+    path('', myHomeView, name='Página de Inicio'),
+    path('another/', anotherView),
     path('admin/', admin.site.urls),
 ]
