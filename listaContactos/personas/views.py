b261e7abb70123ecc7c29594a004d795f5ae4e45
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-13 12:13:15 -0500

    Corrigiendo error sintactico en el diccionario se uso el = en ves de :, además en test.html se modifico el extraer la informacion traida de las vistas, debido que ahora la vista envia un objeto entero

--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -6,6 +6,6 @@ from .models import Persona
 def personaTestView(request):
     obj = Persona.objects.get(id = 1)
     context = {
-        'objeto' = obj,
+        'objeto': obj,
     }
     return render(request, 'personas/test.html', context)
