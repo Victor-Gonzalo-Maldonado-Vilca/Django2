3a47da122da44271874edcf862fcd6b68020ac9f
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-13 12:54:49 -0500

    En el archivo urls.py del proyecto se añadio la ruta para la vista personaCreateView en urlpatterns, donde la ruta es 'agregar/'

--- listaContactos/listaContactos/urls.py
+++ listaContactos/listaContactos/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView, anotherView
-from personas.views import personaTestView
+from personas.views import personaTestView, personaCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', myHomeView, name='Página de Inicio'),
     path('another/', anotherView, name='otro'),
-    path('persona/', personaTestView, name='otro'),
+    path('persona/', personaTestView, name='testViewPersona'),
+    path('agregar/', personaCreateView, name='createPersona')
 ]
