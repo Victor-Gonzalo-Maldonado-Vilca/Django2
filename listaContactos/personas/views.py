46a744f070f6daac8f1e929f9b4c6bde1e4b191f
Victor-Gonzalo-Maldonado-Vilca <vmaldonadov@unsa.edu.pe>
2024-06-20 11:44:56 -0500

    Agregando a la vista verificaciones que corresponden a imprimir los errores que se encuentren, ademas de imprimir los datos cuando sea valido el formulario - se implemente el guardar los datos enviados del formulario

--- listaContactos/personas/views.py
+++ listaContactos/personas/views.py
@@ -7,6 +7,11 @@ def personasAnotherCreateView(request):
     form = RawPersonaForm()
     if request.method == "POST":
         form = RawPersonaForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            Persona.objects.create(**form.cleaned_data)
+        else:
+            print(form.errors)
     context = {
         'form': form,
     }
